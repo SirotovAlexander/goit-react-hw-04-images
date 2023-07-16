a066052afab078570c2435e782eff345f68c87fc
SirotovAlexander <alexander.sirotov@gmail.com>
2023-07-16 21:05:19 +0300

    fix button 'load morel
    fix button 'load morel

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,22 +1,16 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+
 import css from './Button.module.css';
 
-export class Button extends Component {
-  render() {
-    return (
-      <>
-        <button
-          className={css.Button}
-          type="button"
-          onClick={this.props.onClick}
-        >
-          Load more
-        </button>
-      </>
-    );
-  }
-}
+export const Button = ({ onClick }) => {
+  return (
+    <>
+      <button className={css.Button} type="button" onClick={onClick}>
+        Load more
+      </button>
+    </>
+  );
+};
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
