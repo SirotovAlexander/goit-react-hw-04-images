ac6cd40085f0761eb010aa32ba38b72316162920
SirotovAlexander <alexander.sirotov@gmail.com>
2023-07-16 21:01:27 +0300

    fix second elem
    img gallery and img gallery item

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,31 +1,28 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import css from './ImageGalleryItem.module.css';
 
-export class ImageGalleryItem extends Component {
-  render() {
-    return (
-      <>
-        {this.props.items.map(item => {
-          const { id, webformatURL, largeImageURL, tags } = item;
-          return (
-            <li
-              key={id}
-              className={css.ImageGalleryItem}
-              onClick={() => this.props.onClick(largeImageURL)}
-            >
-              <img
-                className={css.ImageGalleryItem_image}
-                src={webformatURL}
-                alt={tags}
-              />
-            </li>
-          );
-        })}
-      </>
-    );
-  }
-}
+export const ImageGalleryItem = ({ items, onClick }) => {
+  return (
+    <>
+      {items.map(item => {
+        const { id, webformatURL, largeImageURL, tags } = item;
+        return (
+          <li
+            key={id}
+            className={css.ImageGalleryItem}
+            onClick={() => onClick(largeImageURL)}
+          >
+            <img
+              className={css.ImageGalleryItem_image}
+              src={webformatURL}
+              alt={tags}
+            />
+          </li>
+        );
+      })}
+    </>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   items: PropTypes.arrayOf(
