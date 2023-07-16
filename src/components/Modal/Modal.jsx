c2f925f3fe733cf7967b7e2ebbd4643e1a5c4bdf
SirotovAlexander <alexander.sirotov@gmail.com>
2023-07-16 22:05:16 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -30,35 +30,6 @@ export const Modal = ({ pic, onClose }) => {
   );
 };
 
-// export class Modal extends Component {
-//   handleEsc = ({ code }) => {
-//     if (code === 'Escape') this.props.onClose();
-//   };
-//   componentDidMount() {
-//     document.addEventListener('keydown', this.handleEsc);
-//   }
-
-//   componentWillUnmount() {
-//     document.removeEventListener('keydown', this.handleEsc);
-//   }
-
-//   handleClick = event => {
-//     if (event.target === event.currentTarget) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     return (
-//       <div className={css.Overlay} onClick={this.handleClick}>
-//         <div className={css.Modal}>
-//           <img src={this.props.pic} alt="some-pic" />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 Modal.propTypes = {
   pic: PropTypes.string.isRequired,
   onClose: PropTypes.func.isRequired,
